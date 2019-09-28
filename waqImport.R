551a281d5ede8c62f1741550ac9750d92611a758
Péter Kalicz <kaliczp@gmail.com>
2019-09-28 18:52:59 +0200

    Comments are translated to English.

--- waqImport.R
+++ waqImport.R
@@ -7,11 +7,11 @@ waqImport <- function(filename, freq.in.sec = 60, trunc.unit="min", Correction =
     if(size == 0) {
         stop("No data in file!")
     }
-    ### Fejléc feldolgozása
+    ### Process header
     newdata <- file(filename, "rb")
     rawHead <- readBin(newdata, "raw", n=158)
     rawDate <- as.character(readBin(newdata, "raw", n=5))
-    ## Még utána van egy 01 és egy 00 szünet
+    ## Process date and time
     StartDate <- paste(paste0("20", rawDate[1]),
                        rawDate[2],
                        rawDate[3],
@@ -25,29 +25,34 @@ waqImport <- function(filename, freq.in.sec = 60, trunc.unit="min", Correction =
     TimeDiff <- difftime(StartDateTime,LoggerStart, units="sec") 
     freq.multi <- ifelse( TimeDiff > freq.in.sec, 2, 1)
     LoggerStart <- LoggerStart + freq.multi * freq.in.sec
-    readBin(newdata, "raw", n=2) ## 01 és 00 eldobása
-    rawDataLength <- readBin(newdata, "integer", size=2) ## Az eltárolt adatok száma!
-    ## 5 db 0, 90, 0, 90, 52, b1, szám MI LEHET???
-    ## Egyelőre eldobom
-    readBin(newdata, "raw", n=11) ## fentiek eldobása
-    ## elválasztó 0
-    readBin(newdata, "raw", n=1) ## fentiek eldobása
+    ## Continue processing with dropping 01 and 00
+    readBin(newdata, "raw", n=2)
+    ## Read number of stored records
+    rawDataLength <- readBin(newdata, "integer", size=2)
+    ## 5 db 0, 90, 0, 90, 52, b1, number What is the function???
+    ## Drop unknown thing
+    readBin(newdata, "raw", n=11)
+    ## Drop separator 0
+    readBin(newdata, "raw", n=1)
+    ## Read end date
     rawEndDate <- as.character(readBin(newdata, "raw", n=5))
+    ## Process end date
     AscEndDateTime <-  paste(paste(paste0("20",rawEndDate[1]),
                 rawEndDate[2],
                 rawEndDate[3],sep="-"),
           paste(rawEndDate[4], rawEndDate[5], sep=":")
           )
+    ## Echo filename, start end end date
     cat(paste(filename, AscStartDateTime, AscEndDateTime, "\n"))
-    ## Utána: bc, 74, 13, 3c , 0, 0, 40, 3f ...
+    ## After end date some unknown code: bc, 74, 13, 3c , 0, 0, 40, 3f ...
     readBin(newdata, "raw", n=76)
     rawData <- readBin(newdata, integer(), size=2, n=rawDataLength)
     close(newdata)
-    ## Idők előállítása
+    ## Generate Date Series
     DateSeries <- seq(LoggerStart,
                       by = paste(freq.in.sec, "sec"),
                       length.out = rawDataLength
                       )
-    ## Az idősor előállítása közben osztás a korrekcióval
+    ## Final xts, data modified with some constant divider.
     xts(rawData/Correction, DateSeries)
 }
